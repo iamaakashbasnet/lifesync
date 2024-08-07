0730cff33430410b877ecd30cae75628c21f547b
jibesh <jibeshshrestha00@gmail.com>
2024-08-07 19:49:32 +0545

    feat: add refresh token request DTO

---
+++ backend/src/main/java/com/example/lifesync/token/RefreshTokenRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.lifesync.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequestDTO {
+
+    private String refreshToken;
+
+}
