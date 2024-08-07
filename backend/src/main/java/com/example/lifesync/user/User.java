f4ae18a6ea57f1be118c345fefadafc90e57d7f1
jibesh <jibeshshrestha00@gmail.com>
2024-08-07 15:38:43 +0545

    feat: add user entity

--- backend/src/main/java/com/example/lifesync/user/User.java
+++ backend/src/main/java/com/example/lifesync/user/User.java
@@ -1,4 +1,41 @@
 package com.example.lifesync.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.management.relation.Role;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USERS")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String password;
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private List<UserRole> roles;
+
+
+    public User(String username, String password, List<UserRole> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public void addAuthority(UserRole authority) {
+        this.roles.add(authority);
+    }
+
 }
