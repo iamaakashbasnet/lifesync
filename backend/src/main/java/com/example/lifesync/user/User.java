70d0b132739cda0eb07257c47d2584c28677d294
jibesh <jibeshshrestha00@gmail.com>
2024-08-08 22:11:53 +0545

    feat: add email support

--- backend/src/main/java/com/example/lifesync/user/User.java
+++ backend/src/main/java/com/example/lifesync/user/User.java
@@ -18,11 +18,14 @@ import java.util.Set;
 @AllArgsConstructor
 @Table(name = "USERS")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     private String username;
+
+    private String email;
 
     private String password;
 
