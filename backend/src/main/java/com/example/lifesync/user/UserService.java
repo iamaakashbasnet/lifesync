fb118b0ffbbd2007eb3db046975aa1d6ba82ee82
jibesh <jibeshshrestha00@gmail.com>
2024-08-08 13:37:12 +0545

    misc: random changes

--- backend/src/main/java/com/example/lifesync/user/UserService.java
+++ backend/src/main/java/com/example/lifesync/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.lifesync.user;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserService {
 
     @Autowired
