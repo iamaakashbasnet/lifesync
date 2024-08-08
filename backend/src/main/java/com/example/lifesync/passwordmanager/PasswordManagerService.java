bf9cb986bf7790f02f2d27f1273110c9d19c3dab
jibesh <jibeshshrestha00@gmail.com>
2024-08-08 16:32:16 +0545

    feat: add password manager service

--- backend/src/main/java/com/example/lifesync/passwordmanager/PasswordManagerService.java
+++ backend/src/main/java/com/example/lifesync/passwordmanager/PasswordManagerService.java
@@ -1,4 +1,26 @@
 package com.example.lifesync.passwordmanager;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class PasswordManagerService {
+
+    private final PasswordManagerRepository passwordManagerRepository;
+
+    public void createPasswordManager(PasswordManager password) {
+        passwordManagerRepository.save(password);
+    }
+
+    public List<PasswordManager> findAll() {
+        return passwordManagerRepository.findAll();
+    }
+
+    public PasswordManager findOne(int id) {
+        return passwordManagerRepository.getReferenceById(id);
+    }
+
 }
