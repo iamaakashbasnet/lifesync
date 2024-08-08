cee675dfe7b865036a84882de806edeb2f2993d8
jibesh <jibeshshrestha00@gmail.com>
2024-08-08 13:35:10 +0545

    feat: add note request dto

---
+++ backend/src/main/java/com/example/lifesync/note/NoteRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.lifesync.note;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NoteRequestDTO {
+
+    private String title;
+    private String content;
+
+}
