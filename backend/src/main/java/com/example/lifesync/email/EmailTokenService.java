e783504c2e7757ec929d995aba017ace54fa7beb
jibesh <jibeshshrestha00@gmail.com>
2024-08-08 22:13:59 +0545

    feat: add email token service

---
+++ backend/src/main/java/com/example/lifesync/email/EmailTokenService.java
@@ -0,0 +1,32 @@
+package com.example.lifesync.email;
+
+import com.example.lifesync.user.User;
+import com.rabbitmq.client.AMQP;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+@Service
+@RequiredArgsConstructor
+public class EmailTokenService {
+
+    private final EmailTokenRepository emailTokenRepository;
+
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public Integer generateToken() {
+        return secureRandom.nextInt(900000) + 100000;
+    }
+
+    public EmailToken getTokenByUser(User user) {
+        return emailTokenRepository.findTokenByUser(user);
+    }
+
+    public EmailToken save(EmailToken emailToken) {
+        emailTokenRepository.save(emailToken);
+        return emailToken;
+    }
+
+}
