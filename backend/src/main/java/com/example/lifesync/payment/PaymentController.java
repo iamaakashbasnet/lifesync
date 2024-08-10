b6066e9749e2cba342382e2e675cad4c42e55dc7
jibesh <jibeshshrestha00@gmail.com>
2024-08-10 11:20:48 +0545

    feat: initial payment skeleton

---
+++ backend/src/main/java/com/example/lifesync/payment/PaymentController.java
@@ -0,0 +1,11 @@
+package com.example.lifesync.payment;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+
+
+}
